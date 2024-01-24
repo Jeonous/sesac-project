c381bc2b587cd0eeea8db56a6d9ccb3296f7b0e6
Jeonous <corea940@gmail.com>
2024-01-24 13:42:10 +0900

    0124 temp commit

---
+++ frontend/react-dobongi/src/header.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header(props) {
+    console.log('props', props)
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB!!!</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB7777
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header title="RREAACT"></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
+}
+
+export default App;
